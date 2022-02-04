d45dccb2b839e46c73d6c042d99d4b2ab26b298c
FireFace1337 <dimakorobitcyn@yandex.ru>
2022-02-04 21:30:16 +0500

    Completed task-1

---
+++ part-3/js/script.js
@@ -0,0 +1,34 @@
+let tabs = document.querySelectorAll('.tabheader__item');
+let tabsContent = document.querySelectorAll('.tabcontent');
+let tabsParent = document.querySelector('.tabheader__items');
+
+function hideTabsContent() {
+    tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(item => {
+        item.classList.remove('tabheader__item_active');
+    });
+}
+
+function showTabsContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+}
+
+hideTabsContent();
+showTabsContent();
+
+tabsParent.addEventListener('click', (event) => {
+    if (event.target.classList.contains('tabheader__item')) {
+        tabs.forEach((item, i) => {
+            if (item == event.target) {
+                hideTabsContent();
+                showTabsContent(i);
+            }
+        });
+    }
+});

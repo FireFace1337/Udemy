73316314c413b9dfc3bf9e4b5d155d327ccaedde
FireFace1337 <dimakorobitcyn@yandex.ru>
2022-02-05 21:43:36 +0500

    Completed task-2

--- part-3/js/script.js
+++ part-3/js/script.js
@@ -1,3 +1,5 @@
+// tabs
+
 let tabs = document.querySelectorAll('.tabheader__item');
 let tabsContent = document.querySelectorAll('.tabcontent');
 let tabsParent = document.querySelector('.tabheader__items');
@@ -32,3 +34,59 @@ tabsParent.addEventListener('click', (event) => {
         });
     }
 });
+
+// timer 
+
+const deadLine = '2022-02-09';
+
+    function getTimeRemaining(endTime) {
+        let total = Date.parse(endTime) - Date.parse(new Date ());
+        let days = Math.floor(total / (1000 * 60 * 60 * 24));
+        let hours = Math.floor(total / (1000 * 60 * 60) % 24);
+        let minutes = Math.floor(total / (1000 * 60) % 60);
+        let seconds = Math.floor ((total / 1000) % 60);
+
+
+        return {
+            total: total,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+    
+
+    function setClock(selector, endTime) {
+        let timer = document.querySelector(selector);
+        let days = timer.querySelector('#days');
+        let hours = timer.querySelector('#hours');
+        let minutes = timer.querySelector('#minutes');
+        let seconds = timer.querySelector('#seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine);

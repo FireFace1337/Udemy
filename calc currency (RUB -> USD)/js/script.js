b310263309996190554b364db3dbfc6e6b356460
Dmitry <57891204+FireFace1337@users.noreply.github.com>
2022-03-05 00:21:45 +0500

    Add js files

---
+++ calc currency (RUB -> USD)/js/script.js
@@ -0,0 +1,20 @@
+const inputRUB = document.querySelector('#rub');
+const inputUSD = document.querySelector('#usd');
+
+inputRUB.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json', 'charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUSD.value = (+inputRUB.value / data.current.usd).toFixed(2);
+            console.log(typeof(inputRUB.value));
+        } else {
+            inputUSD.value = 'Что-то пошло не так :(';
+        }
+    });
+});

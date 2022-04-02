0f26aca5a759bb310e840bb2adf180b5ffe1913d
Dmitry <dimakorobitcyn@yandex.ru>
2022-04-02 17:57:22 +0500

    Create components

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+// компонента #1
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+// компонента #2 
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+//   return <input 
+//           placeholder={holder} 
+//           type="text" 
+//           style={styledField}/>
+// }
+
+// Компонента #2 Классовый подход (а не функциональный)
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styledField = {
+      width: '300px'
+    };
+
+    return <input 
+            placeholder={holder} 
+            type="text" 
+            style={styledField}/>
+  }
+}
+// Классовый подход (а не функциональный)
+
+// компонента #3
+function Btn() {
+  const text = 'Log in';
+  const logged = false;
+  // const res = () => {
+  //   return 'Log in';
+  // }
+  // const p = <p>Log in</p>;
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-// const elem = <h2>Hello World!</h2>;
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello World!');
-
-const text = 'Hello World!';
-
-const elem = (
-  <div>
-      <h2 className='text'>Текст: {text}</h2>
-      <input type="text" />
-      <button tabIndex="0">Click</button>
-  </div>
-);
+import {Header} from './App';
 
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
 

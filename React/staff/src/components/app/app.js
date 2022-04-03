c26e2c21b469de2996e6548299a32eabb18763b8
Dmitry <dimakorobitcyn@yandex.ru>
2022-04-04 00:26:19 +0500

    Create components in Staff App

---
+++ React/staff/src/components/app/app.js
@@ -0,0 +1,25 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import StaffList from '../staff-list/staff-list';
+import StaffAddForm from '../staff-add-form/staff-add-form';
+
+import './app.css';
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <StaffList/>
+            <StaffAddForm/>
+        </div>
+    );
+}
+
+export default App;

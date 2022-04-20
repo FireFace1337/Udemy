0fbccf15031c2fe06d5ba7a77cee4593c27fba4e
Dmitry <dimakorobitcyn@yandex.ru>
2022-04-20 21:49:07 +0500

    Complete task1-task4

---
+++ JS/Tasks/task3.js
@@ -0,0 +1,73 @@
+// Место для первой задачи
+function firstTask() {
+    for (let i = 5; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+firstTask();
+
+// Место для второй задачи
+function secondTask() {
+    for (let i = 20; i > 9; i--) {
+        console.log(i);
+        if (i === 13) {
+            break;
+        }
+    }
+}
+
+secondTask();
+
+// Место для третьей задачи
+function thirdTask() {
+    for (let i = 2; i < 11; i = i + 2) {
+        console.log(i);
+    }
+}
+
+thirdTask();
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    let i = 2;
+    while (i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+        } else {
+            console.log(i);
+            i++;
+        }
+    }
+}
+
+fourthTask();
+
+// Место для пятой задачи
+
+function fifthTask() {
+    let arrayOfNumbers = [];
+    
+    let j = 5;
+    for (let i = 0; i < 6; i++) {
+        arrayOfNumbers[i] = j;
+        j++;
+    }
+    
+    // Не трогаем
+    return arrayOfNumbers;
+}
+    
+fifthTask();
+
---
+++ JS/Tasks/task4.js
@@ -0,0 +1,46 @@
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    // Не трогаем
+    return result;
+}
+
+firstTask()
+
+// Место для второй задачи
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        } else {
+            data[i] = data[i] + ' - done';
+        }
+    }
+    // Не трогаем
+    return data;
+}
+
+secondTask();
+
+
+// Место для третьей задачи
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 0; i < data.length; i++) {
+        result[i] = data[data.length - 1 - i]
+    }  
+    // Не трогаем
+    return result;
+}
+
+thirdTask();
